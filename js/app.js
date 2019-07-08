d8766d9540dad74de75b163ed38ad327e9fd2521
Thierry Legros <thierry.legros2@club.fr>
2019-07-08 16:21:12 +0200

    random sur les cartes sans doubles possibles

--- js/app.js
+++ js/app.js
@@ -1,14 +1,82 @@
-const cards = document.querySelectorAll('.card');
+// const cards = document.querySelectorAll('.card');
 
-const myInterval = setInterval(function() {
-    for (let card of cards) {
-        if (!card.classList.contains('stop')) {
-            card.classList.toggle('flipped');
-        }
+// const myInterval = setInterval(function() {
+//     for (let card of cards) {
+//         if (!card.classList.contains('stop')) {
+//             card.classList.toggle('flipped');
+//         }
+//     }
+// }, 1000);
+
+
+// setTimeout(function() {
+//     clearInterval(myInterval);
+// }, 30000);
+
+const NB_CARDS = 6;
+
+const cards = [];
+const tabRandomNB = [];
+
+// Methode 1
+function init() {
+    for (let i = 0; i < NB_CARDS; i++) {
+        createCard();
     }
-}, 1000);
+}
 
+function createCard() {
+    const card = document.createElement('div');
+    const backCard = document.createElement('div');
+    const frontCard = document.createElement('div');
 
-setTimeout(function() {
-    clearInterval(myInterval);
-}, 30000);
+    // card.classList.toggle('flipped'); //TMP
+
+    card.appendChild(backCard);
+    card.appendChild(frontCard);
+
+    card.className = 'card';
+    backCard.classList.add('back');
+    frontCard.classList.add('front');
+
+    cards.push(card);
+
+    handleCard(cards[cards.length - 1]);
+
+    document.querySelector('.wrapper').appendChild(card);
+}
+
+function handleCard(card) {
+    let randomNB;
+
+    do {
+        randomNB = Math.ceil(Math.random() * 20);
+
+    } while (tabRandomNB.includes(randomNB));
+
+    tabRandomNB.push(randomNB);
+    card.querySelector('.front').textContent = randomNB;
+
+    card.addEventListener('click', function() {
+        this.classList.toggle('flipped');
+    });
+
+}
+
+init();
+
+// Methode 2
+// function createCard() {
+//     const card = `
+//     <div class="card">
+//     <div class="back"></div>
+//     <div class="front">14</div>
+// </div>
+// `;
+
+//     document.querySelector('.wrapper').innerHTML = card;
+// }
+
+// createCard();
+// createCard();
+// createCard();

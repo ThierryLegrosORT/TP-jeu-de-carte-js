6ee4b0a7ff5433fff741b777748479181c81b113
Thierry Legros <thierry.legros2@club.fr>
2019-07-08 13:38:20 +0200

    réglage mouvement des cartes + affichage chiffre

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@ const cards = document.querySelectorAll('.card');
 
 const myInterval = setInterval(function() {
     for (let card of cards) {
-        card.classList.toggle('flipped');
+        if (!card.classList.contains('stop')) {
+            card.classList.toggle('flipped');
+        }
     }
 }, 1000);
 
 
 setTimeout(function() {
     clearInterval(myInterval);
-}, 10000);
+}, 30000);

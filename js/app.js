02cab5ef4a454c24b904e74acb22d8e32c9cfd78
Thierry Legros <thierry.legros2@club.fr>
2019-07-10 11:36:51 +0200

    repérage des paires de cartes

--- js/app.js
+++ js/app.js
@@ -1,87 +1,92 @@
-// const cards = document.querySelectorAll('.card');
-
-// const myInterval = setInterval(function() {
-//     for (let card of cards) {
-//         if (!card.classList.contains('stop')) {
-//             card.classList.toggle('flipped');
-//         }
-//     }
-// }, 1000);
-
-
-// setTimeout(function() {
-//     clearInterval(myInterval);
-// }, 30000);
+// 1: Création des cartes
+// 2: Génération des nombres aléatoires
+// 3: Unicité des nombres
+// 4: Création des doublons (pairs de carte)
+// 5: Mélange des cartes
+// 6: Affichage des cartes
 
 const NB_CARDS = 10;
 
 const cards = [];
 const tabRandomNB = [];
 
-// Methode 1
+let firstFlippedCard = null;
+
 function init() {
     for (let i = 0; i < NB_CARDS; i++) {
         createCard();
     }
 }
 
-
 function createCard() {
     const card = document.createElement('div');
     const backCard = document.createElement('div');
     const frontCard = document.createElement('div');
 
-
     card.appendChild(backCard);
     card.appendChild(frontCard);
 
     card.classList.add('card');
-    // card.className = 'card';
     backCard.classList.add('back');
     frontCard.classList.add('front');
 
     cards.push(card);
     handleCard(cards.length - 1);
-
-    document.querySelector('.wrapper').appendChild(card);
 }
 
 function handleCard(index) {
     const card = cards[index];
+
+    addClickListener(card);
+    generateRandomNB(card, index);
+    shuffleCards();
+    displayCards();
+}
+
+function addClickListener(card) {
+    card.addEventListener('click', function() {
+        card.classList.toggle('flipped');
+
+        if (firstFlippedCard) {
+            if (firstFlippedCard.textContent === this.textContent) {
+                console.log('ok');
+            } else {
+                console.log('pas ok');
+            }
+
+            firstFlippedCard = null;
+        } else {
+
+            firstFlippedCard = this;
+        }
+    });
+}
+
+function generateRandomNB(card, index) {
     let randomNB = tabRandomNB[tabRandomNB.length - 1];
 
     if (index % 2 === 0) {
         do {
             randomNB = Math.ceil(Math.random() * 20);
-
-        } while (tabRandomNB.includes(randomNB));
+        }
+        while (tabRandomNB.includes(randomNB));
 
         tabRandomNB.push(randomNB);
-
     }
 
     card.querySelector('.front').textContent = randomNB;
+}
 
-    card.addEventListener('click', function() {
-        this.classList.toggle('flipped');
+function shuffleCards() {
+    cards.sort(function() {
+        return Math.random() - 0.5;
     });
+}
 
+function displayCards() {
+    cards.forEach(function(card) {
+        document.querySelector('.wrapper').appendChild(card);
+    });
 }
 
 init();
-
-// Methode 2
-// function createCard() {
-//     const card = `
-//     <div class="card">
-//     <div class="back"></div>
-//     <div class="front">14</div>
-// </div>
-// `;
-
-//     document.querySelector('.wrapper').innerHTML = card;
-// }
-
-// createCard();
-// createCard();
-// createCard();
